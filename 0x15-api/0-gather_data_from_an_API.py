39e8925a210d16ee802dd347dbb688e18acb5df1
samiyonas <samuel.x.yonas@gmail.com>
2024-06-12 21:43:07 +0300

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,22 +4,23 @@ import requests
 from sys import argv
 
 
-res_1 = requests.get("https://jsonplaceholder.typicode.com/users")
-res_2 = requests.get("https://jsonplaceholder.typicode.com/todos")
+if __name__ == "__main__":
+    res_1 = requests.get("https://jsonplaceholder.typicode.com/users")
+    res_2 = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-res_1 = res_1.json()
-res_2 = res_2.json()
+    res_1 = res_1.json()
+    res_2 = res_2.json()
 
-Id = int(argv[1])
-name = res_1[Id - 1].get("name")
-tasks = 0
-titles = []
+    Id = int(argv[1])
+    name = res_1[Id - 1].get("name")
+    tasks = 0
+    titles = []
 
-for i in range((Id - 1) * 20, ((Id - 1) * 20) + 20):
-    if res_2[i].get("completed") is True:
-        tasks += 1
-        titles.append(res_2[i].get("title"))
+    for i in range((Id - 1) * 20, ((Id - 1) * 20) + 20):
+        if res_2[i].get("completed") is True:
+            tasks += 1
+            titles.append(res_2[i].get("title"))
 
-print("Employee {} is done with tasks({}/20):".format(name, tasks))
-for i in titles:
-    print("\t {}".format(i))
+    print("Employee {} is done with tasks({}/20):".format(name, tasks))
+    for i in titles:
+        print("\t {}".format(i))
